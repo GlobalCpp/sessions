f0e6f2904be8363efefb318d6db4f922972aad06
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-01-31 11:34:38 -0600

    simple loop

--- src/investigate.cpp
+++ src/investigate.cpp
@@ -1,7 +1,28 @@
-#include <omp.h>
 #include <print>
+#include <vector>
+
+// include openmp library
+#include <omp.h>
 
 int main() {
-  std::println( "{}",omp_get_num_procs() );
+
+  auto nprocs = omp_get_num_procs();
+  std::println( "{}",nprocs );
+
+#pragma omp parallel
+  {
+    auto nthreads = omp_get_num_threads(),
+      tnum = omp_get_thread_num();
+    if (tnum==0) 
+      std::println( "{}",nthreads );
+  }
+  
+  std::vector<double> v(10,2);
+#pragma omp parallel for 
+    for ( auto& x : v )
+      x = x*x;
+
+  std::println( "{}",v );
+
   return 0;
 }
